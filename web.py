bf22ec66826140ae6a7aec939284bb7c8bf0ec1c
lungsang-bod <lungsang@esukhia.org>
2022-06-20 12:29:09 +0530

    first

---
+++ web.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# STREAMLIT DEMO
+""")
+
+file = st.file_uploader("UPLOAD RAW FILE")
+application = st.sidebar.text_input('application')
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+train_df = pd.read_csv("train.csv")
+st.dataframe(train_df.head())
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
